ad27977d38d5d6614a51e2b30573ea9bb7639715
jenkins <levi.stephen@gmail.com>
2015-03-13 15:17:15 -0700

    Pass through CI environment variable.
    Coveralls requires some kind of indication it's in a CI environment.

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe Construi::Config do
       YAML
     end
 
-    let (:config) { Construi::Config.load(config_content) }
+    let(:config) { Construi::Config.load(config_content) }
 
     subject { config.image }
 
     %w{ test-image:latest lstephen/construi:latest }.each do |image_name|
       context "when image is #{image_name}" do
-        let (:image) { image_name }
+        let(:image) { image_name }
         it { is_expected.to eq(image) }
       end
     end
