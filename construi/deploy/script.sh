d70d38daf9b0bd18e1ad60a45087a51c91a2c1fe
lstephen <levi.stephen@gmail.com>
2015-03-19 12:55:57 -0700

    Fix error in deploy script

--- construi/deploy/script.sh
+++ construi/deploy/script.sh
@@ -10,7 +10,7 @@ chmod 0600 ~/.ssh/id_rsa
 printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git config push.default simple
 mkdir -p ~/.gem
-"printf -- \"---\\n:rubygems_api_key: ${RUBYGEMS_API_KEY}\" >> ~/.gem/credentials"
+printf -- \"---\\n:rubygems_api_key: ${RUBYGEMS_API_KEY}\" >> ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 bundle install --path vendor/bundle
 bundle exec gem release --tag
