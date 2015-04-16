1011dee60d923529c43bb80b2d97e9c6c04e3e06
lstephen <levi.stephen@gmail.com>
2015-04-15 17:04:17 -0700

    Reduce complexity in Construi::Runner#run

--- lib/construi/runner.rb
+++ lib/construi/runner.rb
@@ -15,22 +15,26 @@ module Construi
       @config = config
     end
 
-    def run(targets)
-      puts "Construi version: #{Construi::VERSION}"
-
+    def setup_docker
       docker_host = ENV['DOCKER_HOST']
-      Docker.url = docker_host unless docker_host.nil?
+      Docker.url = docker_host if docker_host
 
       puts "Docker url: #{Docker.url}"
-      puts "Current directory: #{Dir.pwd}"
 
       Docker.validate_version!
       Docker.options[:read_timeout] = 60
       Docker.options[:chunk_size] = 8
+    end
+
+    def run(targets)
+      puts "Construi version: #{Construi::VERSION}"
+
+      setup_docker
+
+      puts "Current directory: #{Dir.pwd}"
 
       targets.map {|t| Target.new t, @config.target(t) } .each(&:run)
     end
-
   end
 
 end
