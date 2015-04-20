28814d04bde0e85aa18f84b26269a687e44b1d75
lstephen <levi.stephen@gmail.com>
2015-04-20 16:01:31 -0700

    Update release/deployment to use files feature
    Also attempt to fix always failing deploy script

--- construi/deploy/script.sh
+++ construi/deploy/script.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-git config user.name lstephen
-git config user.email levi.stephen@gmail.com
-mkdir -p ~/.ssh
-cp ${GIT_SSH_KEY} ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
+[[ -n $GIT_AUTHOR_NAME ]] && git config user.name $GIT_AUTHOR_NAME
+[[ -n $GIT_AUTHOR_EMAIL ]] && git config user.email $GIT_AUTHOR_EMAIL
+
 printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git config push.default simple
+
 mkdir -p ~/.gem
 printf -- "---\\n:rubygems_api_key: ${RUBYGEMS_API_KEY}" >> ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
+
+git branch --set-upstream $GIT_BRANCH origin/master
+
 bundle install --path vendor/bundle
 bundle exec gem release --tag
 
--- construi/release/script.sh
+++ construi/release/script.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-git config user.name lstephen
-git config user.email levi.stephen@gmail.com
-mkdir -p ~/.ssh
-cp ${GIT_SSH_KEY} ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
+[[ -n $GIT_AUTHOR_NAME ]] && git config user.name $GIT_AUTHOR_NAME
+[[ -n $GIT_AUTHOR_EMAIL ]] && git config user.email $GIT_AUTHOR_EMAIL
+
 printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
 git config push.default simple
 git checkout master
 git pull --rebase
-echo ${GIT_COMMIT}
 git merge --commit ${GIT_COMMIT}
 git push origin
 git checkout develop
