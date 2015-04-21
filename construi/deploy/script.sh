d656cffb4b723f5e781e747438e78ef9803fcfc3
lstephen <levi.stephen@gmail.com>
2015-04-20 17:14:32 -0700

    Apply fixes tested on master

--- construi/deploy/script.sh
+++ construi/deploy/script.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-[[ -n $GIT_AUTHOR_NAME ]] && git config user.name $GIT_AUTHOR_NAME
-[[ -n $GIT_AUTHOR_EMAIL ]] && git config user.email $GIT_AUTHOR_EMAIL
+[[ -n "$GIT_AUTHOR_NAME" ]] && git config user.name $GIT_AUTHOR_NAME
+[[ -n "$GIT_AUTHOR_EMAIL" ]] && git config user.email $GIT_AUTHOR_EMAIL
 
 printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
