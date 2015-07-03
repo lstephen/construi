a4768a0ace32dbf682d42543cf7934c98de9d32c
lstephen <levi.stephen@gmail.com>
2015-07-02 21:58:08 -0700

    Improvements to release script

--- construi/release/script.sh
+++ construi/release/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 [[ -n "$GIT_AUTHOR_NAME" ]] && git config user.name $GIT_AUTHOR_NAME
 [[ -n "$GIT_AUTHOR_EMAIL" ]] && git config user.email $GIT_AUTHOR_EMAIL
@@ -11,17 +12,18 @@ release_commit=`git rev-parse HEAD`
 
 echo "Release commit ${release_commit}..."
 
+bundle install --path vendor/bundle
+
 echo "Pushing to master..."
 git push -f origin `git rev-parse HEAD`:master
 
 echo "Push to master done."
 
 echo "Updating development version..."
-bundle install --path vendor/bundle
 bundle exec gem bump --version minor
 
 echo "Pushing to develop..."
-git push origin `get rev-parse HEAD`:develop
+git push origin `git rev-parse HEAD`:develop
 echo "Push to develop done."
 
 echo "Release done."
