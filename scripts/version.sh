9366e79d12c72820090a83abeca0c545a3a15a58
lstephen <levi.stephen@gmail.com>
2016-01-06 15:19:13 -0800

    Release target

---
+++ scripts/version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+version=$(git log --oneline --first-parent master | wc -l | xargs)
+
+branch=$(git branch | grep '*')
+
+if [[ ! $branch =~ "master$" ]]
+then
+  branch_count=$(git log --oneline --first-parent | wc -l | xargs)
+  version="$version.dev$branch_count"
+fi
+
+output=${1-/dev/stdout}
+
+echo "__version__ = '$version'" > $output
